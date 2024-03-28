e0a35110395fedc25d3db5c5c653701834f2d903
AlvBrayan <137405058+AlvBrayan@users.noreply.github.com>
2024-03-28 10:21:00 +0100

    Add files via upload

---
+++ LR-TGV/Test_Data/AcqParam.m
@@ -0,0 +1,9 @@
+Bandwidth = 7142.85714; % ##$SW_h=
+FidPoints = 1024; % ##$ACQ_size=( 3 )
+AcqFreqShift = 1200; % ##$ACQ_size=( 3 )
+WaterAcqFreqShift = 1200; % ##$ACQ_size=( 3 )
+NMRFreq =599.418307546438;% ##$BF1=599.418307546438
+NA = 1; % ##$ACQ_size=( 3 );
+MatSize(1) = 31;
+MatSize(2) = 31; % ##$ACQ_spatial_size_0, ##$ACQ_spatial_size_1
+
